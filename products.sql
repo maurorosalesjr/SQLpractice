8afa97ebb9cd28242d3ee81c9fb00150bfca57d3
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-11-13 12:24:57 -0800

    add id column

--- products.sql
+++ products.sql
@@ -18,9 +18,16 @@
 -- VALUES (12.99, 'A Book', 'This is a book with pages and stuff', 39, 'uploads/images/products/a-book.jpg')
 
 -- Task 5: add contraints
-ALTER TABLE products 
-MODIFY COLUMN name VARCHAR(200) NOT NULL,
-MODIFY COLUMN price NUMERIC(10,2) NOT NULL CHECK (price > 0),
-MODIFY COLUMN description TEXT NOT NULL,
-MODIFY COLUMN stock INT CHECK(stock >= 0);
+-- ALTER TABLE products 
+-- MODIFY COLUMN name VARCHAR(200) NOT NULL,
+-- MODIFY COLUMN price NUMERIC(10,2) NOT NULL CHECK (price > 0),
+-- MODIFY COLUMN description TEXT NOT NULL,
+-- MODIFY COLUMN stock INT CHECK(stock >= 0);
+
+-- Taskt 6: add id column 
+
+ALTER TABLE products
+ADD COLUMN id INT PRIMARY KEY AUTO_INCREMENT;
+
+
 
