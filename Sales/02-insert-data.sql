3c1891b44ed3de4c5ecd3268f2f2e38ac5c9928d
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-11-13 15:35:23 -0800

    adding multiple data sets

--- Sales/02-insert-data.sql
+++ Sales/02-insert-data.sql
@@ -1,3 +1,28 @@
 -- INSERT INTO sales (customer_name, product_name, volume, is_recurring)
 
 -- VALUES ('Max Schwarz', 'A book', 12.99, TRUE);
+
+INSERT INTO sales (
+  date_fulfilled,
+  customer_name, 
+  product_name, 
+  volume, 
+  is_recurring,
+  is_disputed
+  )
+
+  VALUES (
+    NULL,
+    'Learning INC',
+    'Course Bundle',
+    4889.62,
+    FALSE,
+    FALSE
+  ), (
+    '2022-04-10',
+    'Big Oil',
+    'Trucks',
+    400000.0,
+    FALSE,
+    TRUE
+  );
