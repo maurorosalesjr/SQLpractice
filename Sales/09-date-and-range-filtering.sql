2a0e972e75b5f9167e32c2f5733de0f14421b0ec
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-11-14 18:53:28 -0800

    basic filtering and date range filtering

---
+++ Sales/09-date-and-range-filtering.sql
@@ -0,0 +1,11 @@
+-- SELECT * FROM sales
+-- WHERE date_created > '2021-11-01' AND date_created < '2022-05-01';
+
+-- SELECT * FROM sales
+-- WHERE date_created BETWEEN '2021-11-01' AND '2022-05-01';
+
+-- SELECT * FROM sales
+-- WHERE volume > 1000 and volume < 100000;
+
+SELECT * FROM sales
+WHERE volume BETWEEN 1000 AND 100000;
